eacabeb5e7655813c6c9090ea1495be290478f96
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2023-08-13 21:25:34 -0500

    Add mocha tests for the Lottery contract
    - Add a get players method to the contract
    - Write tests for contract deployment and entering account

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,3 +1,39 @@
 const ganache = require('ganache');
+const { beforeEach, interfaces, describe } = require('mocha');
 const { Web3 } = require('web3');
-// updated imports added for convenience
+
+const assert = require('assert');
+const { interface, bytecode } = require('../compile');
+
+const web3 = new Web3(ganache.provider());
+
+let lottery;
+let accounts;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    lottery = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode })
+        .send( {from : accounts[0], gas : "1000000" })
+});
+
+describe('Lottery Contract', () => {
+    it("deploys a contract", () => {
+        assert.ok(lottery.options.address);
+    })
+
+    it("allows one account to enter", async() => {
+        await lottery.methods.enter().send({
+            from: accounts[1],
+            value: web3.utils.toWei("0.02", "ether")
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[1], players[0]);
+        assert.equal(1, players.length);
+    });
+})
